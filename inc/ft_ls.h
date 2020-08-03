f1b2cfaa5a77be2f79b5957e511a1b31f1574390
Jong Bloodaxe <jbloodax@mi-c3.msk.21-school.ru>
2020-08-03 13:49:56 +0300

    init commit

---
+++ inc/ft_ls.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbloodax <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 13:42:11 by jbloodax          #+#    #+#             */
+/*   Updated: 2020/08/03 13:42:44 by jbloodax         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include "libft.h"
+
+# include <dirent.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <grp.h>
+# include <uuid/uuid.h>
+# include <time.h>
+# include <sys/types.h>
+# include <sys/acl.h>
+# include <sys/xattr.h>
+# include <limits.h>
+# include <sys/ioctl.h>
+
+typedef struct stat		t_stat;
+
+typedef struct			s_file
+{
+	blkcnt_t			st_blocks;
+	mode_t				mode;
+	nlink_t				st_nlink;
+	uid_t				st_uid;
+	gid_t				st_gid;
+	off_t				size;
+	dev_t				st_rdev;
+	time_t				time;
+	long				ntime;
+	char				*name;
+	char				full_path[PATH_MAX];
+	struct s_file		*next;
+}						t_file;
+
+
+
+
+
+#endif
